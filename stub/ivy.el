6b281c87df741728fe9547f6ad6b25c1d83d6d92
Radon Rosborough <radon.neon@gmail.com>
2018-05-11 12:10:00 -0600

    Update stub/ivy.el

--- stub/ivy.el
+++ stub/ivy.el
@@ -1,11 +1,15 @@
 ;; This file contains stub definitions from ivy.el which allow
 ;; ivy-prescient.el to be byte-compiled in the absence of ivy.el.
 
+(defvar ivy-highlight-functions-alist nil)
+(defvar ivy-minibuffer-faces nil)
 (defvar ivy-re-builders-alist nil)
 (defvar ivy-sort-functions-alist nil)
+(defvar ivy-text nil)
 
 (defun ivy--sort-function (collection))
 (defun ivy--sort-maybe (collection))
+(defun ivy-add-face-text-property (start end face str))
 (defun ivy-read (prompt collection &rest args))
 
 (provide 'ivy)
