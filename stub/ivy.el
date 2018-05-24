61cbe1632cb4370423f441cd78b74b0fd17e2834
Radon Rosborough <radon.neon@gmail.com>
2018-05-23 23:16:08 -0600

    Remove superfluous entries from stub/ivy.el

--- stub/ivy.el
+++ stub/ivy.el
@@ -2,15 +2,9 @@
 ;; ivy-prescient.el to be byte-compiled in the absence of ivy.el.
 
 (defvar ivy--subexps nil)
-(defvar ivy-highlight-functions-alist nil)
-(defvar ivy-minibuffer-faces nil)
 (defvar ivy-re-builders-alist nil)
 (defvar ivy-sort-functions-alist nil)
-(defvar ivy-text nil)
 
-(defun ivy--sort-function (collection))
-(defun ivy--sort-maybe (collection))
-(defun ivy-add-face-text-property (start end face str))
 (defun ivy-read (prompt collection &rest args))
 
 (provide 'ivy)
