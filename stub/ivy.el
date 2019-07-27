58dd61ae7fbe0a207f11682b732af842c0b37d56
Radon Rosborough <radon.neon@gmail.com>
2019-07-27 14:23:52 -0700

    Sort stub/ivy.el

--- stub/ivy.el
+++ stub/ivy.el
@@ -1,19 +1,19 @@
 ;; This file contains stub definitions from ivy.el which allow
 ;; ivy-prescient.el to be byte-compiled in the absence of ivy.el.
 
+(defvar ivy--directory nil)
 (defvar ivy--subexps nil)
 (defvar ivy-initial-inputs-alist nil)
+(defvar ivy-last nil)
+(defvar ivy-marked-candidates nil)
 (defvar ivy-re-builders-alist nil)
 (defvar ivy-sort-functions-alist nil)
-(defvar ivy-marked-candidates nil)
-(defvar ivy-last nil)
-(defvar ivy--directory nil)
 
-(defun ivy-read (prompt collection &rest args))
-(defun ivy-state-sort (struct))
-(defun ivy-state-collection (struct))
-(defun ivy--sort-function (collection))
-(defun ivy--get-action (collection))
 (defun ivy--directory-enter ())
+(defun ivy--get-action (collection))
+(defun ivy--sort-function (collection))
+(defun ivy-read (prompt collection &rest args))
+(defun ivy-state-collection (struct))
+(defun ivy-state-sort (struct))
 
 (provide 'ivy)
